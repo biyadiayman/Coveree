80080c94ef41fa253f12576fb4a911c80bb51afd
BIYADI Mohamed Ayman <biyadi.m.ayman@gmail.com>
2020-07-13 13:53:45 +0200

    Added Price route that accepts json data

--- TestAPI/index.js
+++ TestAPI/index.js
@@ -1,11 +1,25 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 // App
 const app = express()
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
 // Routes
 app.get('/', (req, res) =>{
     res.json({ message: 'Hola Mundo!'})
+})
+
+app.get('/price', (req, res) => {
+    console.log("*** REQ BODY ***");
+    console.log(req.body)
+    console.log(req.body['car'])
+    console.log("*** REQ BODY ***");
+    res.json({price: req.body['salary'] + 69.420})
 })
 
 // Start server
